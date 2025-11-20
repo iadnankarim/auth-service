ca0f61ee5e1be7115945f19fbd7c25ecb12027c2
Adnan Karim <adnankarim725@gmail.com>
2025-11-21 01:31:45 +0500

    auth service using microservice

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js"],
+
+    ignores: ["dist", "node_modules"],
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+  },
+];
